931ed010f78527b4687518f4f3f4d12fc178ecce
arfinzz <khanarfin2000@gmail.com>
2023-06-01 04:26:38 +0530

    implemented routing through url

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 const http=require('http');
 
 const server=http.createServer((req,res)=>{
-    console.log("HI, I am Arfin")
+
+    const url=req.url;
+
+    if(url==="/home")
+    {
+        res.setHeader('content-type','text/html');
+        res.write('<html><head><title>Home</title></head><body><h1>Welcome home</h1></body></html>');
+        return res.end();
+    }
+    else if(url==="/about")
+    {
+        res.setHeader('content-type','text/html');
+        res.write('<html><head><title>About Us</title></head><body><h1>Welcome to About Us Page</h1></body></html>');
+        return res.end();
+    }
+    else if(url==="/node")
+    {
+        res.setHeader('content-type','text/html');
+        res.write('<html><head><title>NodeJS</title></head><body><h1>Welcome to NodeJS Page</h1></body></html>');
+        return res.end();
+    }
+    
+
+    
 })
 
 server.listen(3000);
